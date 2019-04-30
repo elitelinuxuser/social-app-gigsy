d23dc88d14342bd0257dd33a19b0abe8776122ab
Lucifer <ramanandsirvi21@gmail.com>
2019-04-30 10:26:38 +0530

    PrivateRoute Added!

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,23 @@
 import React from "react";
-import { BrowserRouter, Link, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Login from "./components/Login/Login";
+import Profile from "./components/Profile/Profile";
+import Homepage from "./components/Homepage/Homepage";
+import PrivateRoute from "./components/PrivateRoute";
 
-import Admin from './components/Admin/Admin';
-import Homepage from './components/Homepage/Homepage';
-import Login from './components/Login/Login';
-import Navbar from './components/Navbar/Navbar';
-import Profile from './components/Profile/Profile';
 import "semantic-ui-css/semantic.min.css";
 import "./App.css";
 
-const App = () => {
+function App() {
   return (
-    <div>
-      <BrowserRouter>
-        <div>
-          <Navbar />
-          <Route path="/login" exact component={Login} />
-          <Route path="/" exact component={Homepage} />
-          <Route path="/admin" exact component={Admin} />
-          <Route path="/login/profile" exact component={Profile} />
-        </div>
-      </BrowserRouter>
-    </div>
+    <Router>
+      <Switch>
+        <PrivateRoute exact path="/" component={Homepage} />
+        <Route exact path="/login" component={Login} />
+        <PrivateRoute exact path="/profile" component={Profile} />
+      </Switch>
+    </Router>
   );
-};
+}
 
 export default App;
--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,10 +1,9 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React, { Component } from "react";
 
-const Login = (props) => {
-  return (
-    <div>In login page</div>
-  )
+class Login extends Component {
+  render() {
+    return <div>Login Page</div>;
+  }
 }
 
 export default Login;
