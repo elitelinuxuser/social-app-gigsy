9ec03b9552f821f439687e85310d519497c50b32
root <root@instance-1.asia-south1-b.c.vscode-beta-239304.internal>
2019-05-01 19:02:22 +0000

    Login with React!

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Login from "./components/Login/Login";
 import Profile from "./components/Profile/Profile";
@@ -6,22 +6,34 @@ import Homepage from "./components/Homepage/Homepage";
 import PrivateRoute from "./components/PrivateRoute";
 import Admin from "./components/Admin/Admin";
 import ProfileFirst from "./components/Profile/ProfileFirst";
+import setAuthToken from "./utils/setAuthToken";
+import store from "./store";
+import { loadUser } from "./actions/auth";
+
 import "semantic-ui-css/semantic.min.css";
 import "./App.css";
 
-function App() {
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
+
+const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+
   return (
     <Router>
       <Switch>
         <PrivateRoute exact path="/" component={Homepage} />
-        <Route path="/login" component={Homepage} />
-        <Route path="/profile" component={Profile} />
+        <Route path="/login" component={Login} />
+        {/* <Route path="/profile" component={Profile} /> */}
         <PrivateRoute path="/profile" component={Profile} />
         <Route path="/admin" component={Admin} />
         <Route path="/profilefirst" component={ProfileFirst} />
       </Switch>
     </Router>
   );
-}
+};
 
 export default App;
