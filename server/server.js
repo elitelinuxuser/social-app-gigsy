4ea283ca971c72d51480a753d5e0ae9f18a73acc
root <root@instance-1.asia-south1-b.c.vscode-beta-239304.internal>
2019-05-01 14:11:41 +0000

    User, Profile and posts api done!

--- server/server.js
+++ server/server.js
@@ -1,8 +1,20 @@
 const express = require("express");
-
+const connectDB = require("../config/db");
+const passport = require("passport");
 const app = express();
 
+//ConnectDB
+connectDB();
+
+//Init Middleware
+app.use(express.json({ extended: false }));
+
 app.get("/", (req, res) => res.send("Hello World"));
+
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/posts", require("./routes/api/posts"));
 
 const port = process.env.PORT || 5000;
 
